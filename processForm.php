9f18d9d1c2b7d51d34b65d156c4e527747a6edca
Darthmaul <meestermaul@live.com>
2012-12-05 22:00:27 -0500

    Updating site files once more
    Latest version so far

--- processForm.php
+++ processForm.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 // Define some constants
@@ -38,3 +39,45 @@ if ( isset($_GET["ajax"]) ) {
 ?>
 
 
+=======
+<?php
+
+// Define some constants
+define( "RECIPIENT_NAME", "Relay for Life Website" );
+define( "RECIPIENT_EMAIL", "andrew_nolan17@yahoo.com" );
+define( "EMAIL_SUBJECT", "Email from Relay for Life Website" );
+
+// Read the form values
+$success = false;
+$senderName = isset( $_POST['senderName'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['senderName'] ) : "";
+$senderEmail = isset( $_POST['senderEmail'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['senderEmail'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+
+// If all values exist, send the email
+if ( $senderName && $senderEmail && $message ) {
+  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
+  $success = mail( $recipient, EMAIL_SUBJECT, $message, $headers );
+}
+
+// Return an appropriate response to the browser
+if ( isset($_GET["ajax"]) ) {
+  echo $success ? "success" : "error";
+} else {
+?>
+<html>
+  <head>
+    <title>Thanks!</title>
+  </head>
+  <body>
+  <?php if ( $success ) echo "<p>Thanks for sending your message! We'll get back to you shortly.</p>" ?>
+  <?php if ( !$success ) echo "<p>There was a problem sending your message. Please try again.</p>" ?>
+  <p>Click your browser's Back button to return to the page.</p>
+  </body>
+</html>
+<?php
+}
+?>
+
+
+>>>>>>> Updating site files once more
